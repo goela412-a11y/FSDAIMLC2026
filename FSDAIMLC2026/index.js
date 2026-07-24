d038517e68de54e1dccc0672816a120728d1c5b6
goela412-a11y <goela412@gmail.com>
2026-07-24 10:20:25 +0530

    JS Function

--- FSDAIMLC2026/index.js
+++ FSDAIMLC2026/index.js
@@ -31,8 +31,50 @@
 //Invoke Immediate Fuction as an Expression (IIFE)
 // (()=>{console.log("Hiii....IIFE")})();
 
-function sum(a,b){
-    return a+b;
+// function sum(a,b){
+//     return a+b;
+// }
+// const result=sum(20,50);
+// console.log("sum"+result);
+
+// function sum(a,b){
+//     return a+b;
+// }
+// function sumwithMsg(clbk,msg){
+//     const result=clbk(20,40);
+//     console.log(msg+":" +result);
+// }
+// sumwithMsg(sum,"Hey using calculation with js")
+
+//// function login(msg,error){
+//     if(error){
+//         console.log("Error is" +error);
+//     }
+//     else{
+//         console.log("Welcome "+msg);
+//     }
+// }
+
+//// function loginVerification(username,password,clbk){
+// if(username=="Alice" && password=="12345"){
+//     clbk("Alice",null);
+// }else{
+//     clbk(null,"Invalid username or password");
+// }
+// }
+// loginVerification("Alice","12345",login);
+
+function generateRandom(callback) {
+    let randomNumber = Math.floor(Math.random() * 100) + 1; 
+
+    console.log("Generated Number:", randomNumber);
+    callback(randomNumber);
 }
-const result=sum(20,50);
-console.log("sum"+result);
+function checkEvenOdd(number) {
+    if(number % 2 === 0){
+        console.log(number +"is Even");
+    }else{
+        console.log(number +"is Odd");
+    }
+}
+generateRandom(checkEvenOdd);
